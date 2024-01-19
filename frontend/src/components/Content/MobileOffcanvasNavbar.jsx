c77ceeb3c7ff7f9b15b79a2b787e9aa8150b3241
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-01-19 18:21:59 +0530

    changed ui for mobile navbar

--- frontend/src/components/Content/MobileOffcanvasNavbar.jsx
+++ frontend/src/components/Content/MobileOffcanvasNavbar.jsx
@@ -12,26 +12,73 @@ const MobileOffcanvasNavbar = () => {
     }
     return (
         <>
-            <nav className="navbar navbar-expand-lg opacity-100 sticky-top p-1 ">
-                <div className="container-fluid">
-                    <div className="d-flex justify-content-start align-items-center">
-                        <div
-                            className="gola ms-2"
-                            style={{ backgroundColor: "rgba(0, 208, 219, 1)" }}
-                        >
-                            <i className="fa-solid fa-code fa-l"></i>
-                        </div>
-                        <form action="/">
-                            <input
-                                type="button"
-                                className="name fs-4 px-3"
-                                value="Algocraft"
-                                style={{ background: "transparent", border: "none" }}
-                            />
-                        </form>
+            <nav className='navbar p-1 sticky-top' >
+                <div className="w-100 d-flex justify-content-between align-items-center">
+                    <div className="hamburger">
+                        <button style={{ background: "transparent", border: 'none' }} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+                            <i class="fa-solid fa-bars fa-2xl"></i>
+                        </button>
                     </div>
+                    <div
+                        className="gola ms-2"
+                        style={{ backgroundColor: "rgba(0, 208, 219, 1)" }}
+                    >
+                        <i className="fa-solid fa-code fa-l"></i>
+                    </div>
+                    <div className="dropstart ">
+                        <a href="/" className="d-flex align-items-center dropdown-toggle text-decoration-none" id="dropdownUser1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://avatars.githubusercontent.com/u/91490304?v=4" alt="" width="32" height="32" className="rounded-circle me-2" />
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                            <li><a className="dropdown-item" href="/">Settings</a></li>
+                            <li><a className="dropdown-item" href="/">Profile</a></li>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <li><a className="dropdown-item" href="/">Sign out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-                    <div>
+            <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabIndex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                <div class="offcanvas-header" >
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" >
+                        <i class="fa-solid fa-xmark fa-xl"></i>
+                    </button>
+                </div>
+                <div class="offcanvas-body">
+                    <div className="py-3">
+                        <a href="/coding-sheets" className={path === '/coding-sheets' ? 'nav-link active' : 'nav-link'} aria-current="page">
+                            <i className="fa-solid fa-network-wired fa-lg px-2" ></i>
+                        </a>
+                    </div>
+                    <div className="py-3">
+                        <a href="/upcoming-contests" className={path === '/upcoming-contests' ? 'nav-link active' : 'nav-link'}>
+                            <i className="fa-solid fa-trophy fa-lg px-2" ></i>
+
+                        </a>
+                    </div>
+                    <div className="py-3">
+                        <a href="/coding-resources" className={path === '/coding-resources' ? 'nav-link active' : 'nav-link'}>
+                            <i className="fa-solid fa-vault fa-lg px-2"></i>
+                        </a>
+                    </div>
+                    <div className="py-3">
+                        <a href="/discussion" className={path === '/discussion' ? 'nav-link active' : 'nav-link'}>
+                            <i className="fa-solid fa-comments fa-lg px-2"></i>
+                        </a>
+                    </div>
+                    <div className="py-3">
+                        <a href="/coding-ide" className={path === '/coding-ide' ? 'nav-link active' : 'nav-link'} >
+                            <i className="fa-solid fa-terminal fa-lg px-2"></i>
+
+                        </a>
+                    </div>
+                </div>
+                <div className="offcanvas-footer d-flex justify-content-center align-items-center p-5">
+                <div>
                         {isDarkTheme ? (
                             <button
                                 className="rounded-square-button"
@@ -52,74 +99,10 @@ const MobileOffcanvasNavbar = () => {
                                 ></i>
                             </button>
                         )}
-
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavDropdown"
-                            aria-controls="navbarNavDropdown"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-
-                    <div className="collapse navbar-collapse w-20" id="navbarNavDropdown">
-                        <ul className="navbar-nav ms-auto ">
-                            <li className="nav-item py-2">
-                                <a href="/coding-sheets" className={path === '/coding-sheets' ? 'nav-link mx-2 text-links active' : 'nav-link mx-2 text-links '} aria-current="page">
-                                    <i className="fa-solid fa-network-wired fa-lg px-2" ></i>
-                                    Coding Sheets
-                                </a>
-                            </li>
-                            <li className="nav-item py-2">
-                                <a href="/upcoming-contests" className={path === '/upcoming-contests' ? 'nav-link mx-2 text-links active ' : 'nav-link mx-2 text-links '}>
-                                    <i className="fa-solid fa-trophy fa-lg px-2" ></i>
-                                    Upcoming contests
-                                </a>
-                            </li>
-                            <li className="nav-item py-2">
-                                <a href="/coding-resources" className={path === '/coding-resources' ? 'nav-link mx-2 text-links active ' : 'nav-link mx-2 text-links '}>
-                                    <i className="fa-solid fa-vault fa-lg px-2"></i>
-
-                                    Coding Resources
-                                </a>
-                            </li>
-                            <li className="nav-item py-2">
-                                <a href="/discussion" className={path === '/discussion' ? 'nav-link mx-2 text-links active ' : 'nav-link mx-2 text-links '}>
-                                    <i className="fa-solid fa-comments fa-lg px-2"></i>
-                                    Discussion Page
-                                </a>
-                            </li>
-                            <li className="nav-item py-2">
-                                <a href="/coding-ide" className={path === '/coding-ide' ? 'nav-link mx-2 text-links active ' : 'nav-link mx-2 text-links  '} >
-                                    <i className="fa-solid fa-terminal fa-lg px-2"></i>
-                                    Coding IDE
-                                </a>
-                            </li>
-                            <li className="nav-item py-2">
-                                <div className="dropdown dropup-center mx-2">
-                                    <a href="/" className="d-flex align-items-center dropdown-toggle text-decoration-none" id="dropdownUser1"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        <img src="https://avatars.githubusercontent.com/u/91490304?v=4" alt="" width="32" height="32" className="rounded-circle me-2" />
-                                        <strong>Dhruvil</strong>
-                                    </a>
-                                    <ul className="mt-2 dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                                        <li><a className="dropdown-item" href="/">Settings</a></li>
-                                        <li><a className="dropdown-item" href="/">Profile</a></li>
-                                        <li>
-                                            <hr className="dropdown-divider" />
-                                        </li>
-                                        <li><a className="dropdown-item" href="/">Sign out</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
                     </div>
                 </div>
-            </nav>
+            </div>
+
         </>
     )
 }
