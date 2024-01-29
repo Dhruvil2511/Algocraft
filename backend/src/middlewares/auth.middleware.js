3015d463efacb55ce4d603b471c24cd96bb07e4e
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-01-29 09:52:29 +0530

    added auth middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+    if (!user) {
+        throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
+
+    next();
+});
