1e8fa3d5830425a0bd543bfea8d4421b7dc8bfa0
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2025-02-26 22:21:41 +0530

    Added server ping route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -24,6 +24,7 @@ const router = Router();
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
+router.route("/").get(serverCheck);
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/verify-email").get(verifyEmail)
